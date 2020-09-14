8855d4212d51aff9a4cb0bd62ad23a3475f1d77e
Stephen Gaffney <stephen.gaffney@ucdconnect.ie>
2020-09-14 16:12:03 +0100

    Fixed typo, dev scripts ready

--- auto_gen_tests_pipeline/9_PIT/1.5.2_PIT_mutate_dev_suites.sh
+++ auto_gen_tests_pipeline/9_PIT/1.5.2_PIT_mutate_dev_suites.sh
@@ -25,7 +25,7 @@ projects_dir="/home/people/12309511/defects4j/framework/projects"
 # Iterate through projects in framework/projects folder
 for project in "${projects[@]}"; do
         
-        tmp_log_dir="/home/people/12309511/scratch/tmp_logging/${project}"
+        tmp_log_dir="/home/people/12309511/scratch/tmp_logging/PIT_dev_mut/${project}"
         mkdir --parents $tmp_log_dir
 
 	p_dir="${projects_dir}/${project}"
@@ -42,10 +42,10 @@ for project in "${projects[@]}"; do
                         
 			# If vid is in trig tests dir then run mut otherwise don't
                         if [ -d /home/people/12309511/triggering_tests/${project}/${bug_id} ]; then
-				job_name="${project}-dev-${bug_id}-PIT-mutation-1.5.2-test"
+				job_name="${project}-dev-${bug_id}-PIT-mutation-1.5.2-Run2"
 
                                 # Run mut HERE
-                                echo "sbatch --exclude=sonic37 -J ${job_name} -o /dev/null -e ${tmp_log_dir}/${job_name}.out 1.5.2_PIT_mutate_dev_suites.sh $project $bug_id"
+                                sbatch --exclude=sonic37 -J ${job_name} -o /dev/null -e ${tmp_log_dir}/${job_name}.out SBATCH_1.5.2_PIT_dev_mutation.sh $project $bug_id
                        fi
 
                 done < "$bugs_file"
