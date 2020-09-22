4ad0950ffcf1ec7c9e3aaac0c45733003e0c31da
Stephen Gaffney <stephen.gaffney@ucdconnect.ie>
2020-09-22 16:32:44 +0100

    Changed job name

--- auto_gen_tests_pipeline/12_analysis/run_analysis_all_bugs.sh
+++ auto_gen_tests_pipeline/12_analysis/run_analysis_all_bugs.sh
@@ -4,7 +4,7 @@ while read -r line; do
 	pid=$(echo $line | cut -d"-" -f1)
 	vid=$(echo $line | cut -d"-" -f2)
 	
-	job_name="${pid}-${vid}-analysis"
+	job_name="${pid}-${vid}-analysis-final-run"
 
 	sbatch -J ${job_name} -o /dev/null -e /dev/null SBATCH_reveal_faults.sh $pid $vid
 
